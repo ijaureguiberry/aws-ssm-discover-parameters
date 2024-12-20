591128671cfeed1757d52f6785e509d1c774a84d
Ignacio Jaureguiberry <ignacio.jaureguiberry@qubika.com>
2024-12-20 10:23:31 -0300

    Add new version build

--- dist/index.js
+++ dist/index.js
@@ -59983,72 +59983,51 @@ module.exports = /*#__PURE__*/JSON.parse('{"name":"@aws-sdk/client-sts","descrip
 /************************************************************************/
 var __webpack_exports__ = {};
 const core = __nccwpck_require__(7484);
-const { SSMClient, GetParametersByPathCommand } = __nccwpck_require__(4736);
+const {
+  SSMClient,
+  GetParametersByPathCommand,
+} = __nccwpck_require__(4736);
 
 async function run() {
-    try {
-        const region = core.getInput('region', { required: true });
-        const envs = core.getInput('envs', { required: true });
-        const paths = core.getInput('paths', { required: true });
-        const taskDefinitionFile = core.getInput('task-definition', { required: true });
-        const envsParts = envs.split(/\s+/);
-        for (let i=0; i<envsParts.length; i++){
-            envsParts[i].trim();
-        }
-        const pathParts = paths.split(/\s+/);
-        const client = new SSMClient({
-            region: region,
-        });
-        let params=[];
-        for (let j=0; j<pathParts.length; j++){
-            const path = pathParts[j].trim();
-            if (path==="")
-                continue;
-            let nextToken = null;
-            while (true){
-                const input = { 
-                    Path: path,
-                    Recursive: true,
-                    ParameterFilters: [],
-                    WithDecryption: true,
-                    MaxResults: Number("10"),
-                    NextToken: nextToken,
-                };
-                const command = new GetParametersByPathCommand(input);
-                const response = await client.send(command);
-                nextToken = response.NextToken;
-                for (let i=0; i<response.Parameters.length; i++){
-                    const param=response.Parameters[i];
-                    const parts = param.Name.split("/");
-                    const name = parts[parts.length - 1];
-                    const secret = {
-                        name: name,
-                        valueFrom: param.ARN,
-                    };
-                    for (let k=0; k<envsParts.length; k++){
-                        const env = envsParts[k];
-                        if (env===secret.name){
-                            params.push(secret);
-                            break;
-                        }
-                    }
-                }
-                if (!nextToken)
-                    break;
-            }
-        }
-        const taskFile = fs.readFileSync(taskDefinitionFile, 'utf8');
-        const parsedFile = JSON.parse(taskFile);
-        parsedFile.containerDefinitions[0].secrets=params;
-        const renderedTask = JSON.stringify(parsedFile, null, 2);
-        fs.writeFileSync("output.json", renderedTask);
-        core.setOutput("task-definition", "output.json");
-    } catch (error) {
-        core.setFailed(error.message);
+  try {
+    const path = core.getInput("path", { required: true });
+    const region = core.getInput("region", { required: true });
+
+    const ssm = new SSMClient({
+      region: region,
+    });
+
+    core.info(`Fetching SSM parameters from ${region}::${path}/*`);
+    let nextToken = "continue";
+    let outputParams = [];
+    while (nextToken) {
+      const command = new GetParametersByPathCommand({
+        Path: path,
+        WithDecryption: true,
+        Query: "Parameters[].[Name,ARN]",
+        NextToken: nextToken !== "continue" ? nextToken : null,
+      });
+      const response = await ssm.send(command);
+
+      const nextParams = response.Parameters.map((parameter) => {
+        const name = parameter.Name.split(path)[1]
+          .toUpperCase()
+          .replaceAll("-", "_");
+        const arn = parameter.ARN;
+        return `${name}=${arn}`;
+      });
+      outputParams = outputParams.concat(nextParams);
+      nextToken = response.NextToken;
     }
+
+    core.setOutput("discovered-secrets", outputParams.join("\n"));
+  } catch (err) {
+    core.setFailed(`Action failed with error ${err}`);
+  }
 }
 
 run();
+
 module.exports = __webpack_exports__;
 /******/ })()
 ;
